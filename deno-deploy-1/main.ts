9be279a2e6d90acf53f9db8a9b3a1caa5ffd9d66
ruedap <ruedap@ruedap.com>
2021-05-28 21:25:00 +0900

    2021/05/28 21:25:00

--- deno-deploy-1/main.ts
+++ deno-deploy-1/main.ts
@@ -5,16 +5,21 @@ addEventListener("fetch", (event) => {
 async function handleRequest(request: Request) {
   const date = new Date();
   const dateString = date.toLocaleDateString("ja-JP", {
+    timeZone: "Asia/Tokyo", // タイムゾーン設定を追加
     weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
   });
   const timeString = date.toLocaleTimeString("ja-JP", {
+    timeZone: "Asia/Tokyo", // タイムゾーン設定を追加
     hour12: false,
   });
+
+  // アクセス時刻としてログ出力
   const message = `${dateString} ${timeString}`;
 
+  console.log(`access: ${message}`);
   const response = new Response(message, {
     headers: {
       "content-type": "text/plain;charset=utf-8",
